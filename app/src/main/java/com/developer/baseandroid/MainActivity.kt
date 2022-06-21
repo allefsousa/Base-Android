aed4ea92476f23ef6b3c756e1e0410ae2bfc12e0
Allef Sousa <allefsousa_1@hotmail.com>
2022-06-20 23:45:48 -0300

    update dependency

--- app/src/main/java/com/developer/baseandroid/MainActivity.kt
+++ app/src/main/java/com/developer/baseandroid/MainActivity.kt
@@ -3,7 +3,6 @@ package com.developer.baseandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.developer.baseandroid.common.viewBinding
-import com.developer.baseandroid.common.withBinding
 import com.developer.baseandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
